2987cef667ef071ada29f903997063a63ddaf7fb
shreyapawar <144965197+shreyapawarmamidi@users.noreply.github.com>
2024-02-12 19:53:14 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+$(document).ready(function () {
+    $(".container")
+      .mouseenter(function () {
+        $(".card").stop().animate(
+          {
+            top: "-90px",
+          },
+          "slow"
+        );
+      })
+      .mouseleave(function () {
+        $(".card").stop().animate(
+          {
+            top: 0,
+          },
+          "slow"
+        );
+      });
+  }); 
+ 
+  const openModalButtons = document.querySelectorAll('[data-modal-target]')
+  const closeModalButtons = document.querySelectorAll('[data-close-button]')
+  const overlay = document.getElementById('overlay')
+  
+  openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const modal = document.querySelector(button.dataset.modalTarget)
+      openModal(modal)
+    })
+  })
+  
+  overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+      closeModal(modal)
+    })
+  })
+  
+  closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const modal = button.closest('.modal')
+      closeModal(modal)
+    })
+  })
+  
+  function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+  }
+  
+  function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+  }
+
+  document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+      const customButton = document.querySelector('#custom-button');
+      customButton.style.display = 'block';
+    }, 2000); // 5000 milliseconds = 5 seconds
+  });
